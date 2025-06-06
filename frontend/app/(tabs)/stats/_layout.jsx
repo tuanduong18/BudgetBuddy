6326a032598ee9a590b947cf3218709a872a3dc6
unknown <tuanduong18001@gmail.com>
2025-06-06 23:10:42 +0800

    feat: New Statistics Feature
    A blank folder to develop this feature in backend using the same
    database
    A simple template to develop UI for this feature in frontend

---
+++ frontend/app/(tabs)/stats/_layout.jsx
@@ -0,0 +1,9 @@
+import { Stack } from "expo-router";
+
+export default function AuthLayout() {
+  return (
+    <Stack initialRouteName="index" screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="index" />
+    </Stack>
+  );
+}
